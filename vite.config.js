37c6b9e4bf920bdb15914df6e0fd18aa6abc3db6
yoshihiro-yaguchi <yoshihirio.yaguchi@smaregi.jp>
2023-06-09 21:42:25 +0900

    react-typescript-reduxの導入

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
@@ -8,9 +9,12 @@ export default defineConfig({
                 "resources/sass/app.scss",
                 "resources/css/app.css",
                 "resources/js/app.js",
+                'resources/ts/src/index.tsx',
             ],
+
             refresh: true,
         }),
+        react(),
     ],
     // Windows、WSL上で実行するときだけ必要か
     server: {
