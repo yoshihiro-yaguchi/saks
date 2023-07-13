b75446935f8d26d1e149209ace92f09226cd163e
yoshihiro-yaguchi <yoshihiro19990307@gmail.com>
2023-07-13 13:25:52 +0900

    validationデータをreactに渡す

--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -5,14 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   {{-- reactデータ連携 --}}
-  @if ($errors->any())
-  @foreach ($errors->all() as $error)
-    <meta name='errors' content="{{$error}}">
-  @endforeach
-  @endif
   <meta name="csrfToken" content={{csrf_token()}}>
-  <meta name="data" content="{{$encodeData}}">
   <meta name="baseUrl" content="{{Request::root()}}">
+  @if (isset($errors))
+    <meta name='errors' content="{{$errors}}">
+  @endif
+  @if (isset($encodeData))
+    <meta name="data" content="{{$encodeData}}">
+  @endif
   <title>取引作成</title>
   @viteReactRefresh
   @vite(['resources/ts/src/features/transaction/create/index.tsx',
@@ -22,11 +22,6 @@
 </head>
 <body class="deliverySlip-create">
   {{-- エラー --}}
-  @if ($errors->any())
-  @foreach ($errors->all() as $error)
-    <input name='errors' hidden readonly value="{{$error}}">
-  @endforeach
-  @endif
   <div class="createTransaction" id="createTransaction"></div>
 </body>
 <script>
