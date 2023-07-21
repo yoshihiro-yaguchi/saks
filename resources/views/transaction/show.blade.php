4352f27153867b7d55bfa21ccde04131594e8226
yoshihiro-yaguchi <yoshihiro19990307@gmail.com>
2023-07-21 16:55:08 +0900

    reactデータ連携

--- resources/views/transaction/show.blade.php
+++ resources/views/transaction/show.blade.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  {{-- reactデータ連携 --}}
+  <meta name="csrfToken" content={{csrf_token()}}>
+  <meta name="baseUrl" content="{{Request::root()}}">
   <title>取引詳細</title>
   @viteReactRefresh
   @vite(['resources/ts/src/features/transaction/show/index.tsx',
