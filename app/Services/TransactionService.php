1eadb64e0cec5a0f5dbdf9f82bd25c7aa4a2872a
yoshihiro-yaguchi <yoshihiro19990307@gmail.com>
2023-07-23 01:04:12 +0900

    取引データ取得して画面表示用に加工する。

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -32,7 +32,7 @@ class TransactionService
       'transaction_pic_first_name' => $transactionInfo['transactionPicFirstName'],
       'transaction_note' => $transactionInfo['transactionNote'],
       'corporation_division' => $customerInfo['corporationDivision'],
-      'invoice_number' => $customerInfo['invoiceNumber'],
+      'customer_invoice_number' => $customerInfo['invoiceNumber'],
       'customer_company' => $customerInfo['customerCompany'],
       'customer_branch' => $customerInfo['customerBranch'],
       'customer_last_name' => $customerInfo['customerLastName'],
@@ -68,13 +68,83 @@ class TransactionService
         'contract_id' => $contractId,
         'transaction_id' => $transactionId,
         'product_no' => $detailRow['productNo'],
-        'production_name' => $detailRow['productName'],
+        'product_name' => $detailRow['productName'],
         'quantity' => $detailRow['quantity'],
         'unit_price' => $detailRow['unitPrice'],
-        'tax_rate' => $detailRow['quantity'],
+        'tax_rate' => $detailRow['taxRate'],
         'total_price' => $detailRow['totalPrice']
       ];
     }
     TransactionDetail::insert($saveData);
+  }
+
+  /**
+   * 取引データ取得
+   *
+   * @param string $contractId
+   * @param string $transactionId
+   * @return array
+   */
+  public function getTransactionData(string $contractId, string $transactionId)
+  {
+    $transactionHeadData = TransactionHead::query()->where('contract_id', '=', $contractId)->where('transaction_id', '=', $transactionId)->first();
+    $transactionInfo = array(
+      'transactionTitle' => $transactionHeadData->transaction_title,
+      'transactionDivision' => $transactionHeadData->transaction_division, // TODO: 定数から日本語データを取得する。
+      'transactionDate' => $transactionHeadData->transaction_date,
+      'transactionBranch' => $transactionHeadData->transaction_branch, // TODO: 支店マスタ作ったらデータ見に行くようにする。
+      'transactionPicName' => $transactionHeadData->transaction_pic_last_name . ' ' . $transactionHeadData->transaction_pic_first_name,
+      'transactionNote' => $transactionHeadData->transaction_note,
+    );
+    $customerInfo = array(
+      'corporationDivision' => $transactionHeadData->corporation_division, // TODO: 定数から日本語データを取得する。
+      'invoiceNumber' => $transactionHeadData->customer_invoice_number,
+      'customerCompany' => $transactionHeadData->customer_company,
+      'customerBranch' => $transactionHeadData->customer_branch,
+      'customerName' => $transactionHeadData->customer_last_name . ' ' . $transactionHeadData->customer_first_name,
+      'customerPhoneNumber' => $transactionHeadData->customer_phone_number,
+      'customerZipCode' => $transactionHeadData->customer_zip_code,
+      'customerAddress1' => $transactionHeadData->customer_address1,
+      'customerAddress2' => $transactionHeadData->customer_address2,
+      'customerAddress3' => $transactionHeadData->customer_address3,
+      'customerAddress4' => $transactionHeadData->customer_address4,
+    );
+    $amountInfo = array(
+      'subtotal' => $transactionHeadData->subtotal,
+      'taxInclude' => $transactionHeadData->tax_include,
+      'total' => $transactionHeadData->total,
+    );
+    $detailRows = $this->getTransactionDetails($contractId, $transactionId);
+
+    return array(
+      'transactionInfo' => $transactionInfo,
+      'customerInfo' => $customerInfo,
+      'amountInfo' => $amountInfo,
+      'detailRows' => $detailRows,
+    );
+  }
+
+  /**
+   * 取引明細データ取得
+   *
+   * @param string $contractId
+   * @param string $transactionId
+   * @return array
+   */
+  private function getTransactionDetails(string $contractId, string $transactionId)
+  {
+    $transactionDetailDatas = TransactionDetail::query()->where('contract_id', '=', $contractId)->where('transaction_id', '=', $transactionId)->get();
+    $detailDatas = array();
+    foreach ($transactionDetailDatas as $detailData) {
+      $detailDatas[] = array(
+        'productNo' => $detailData->product_no,
+        'productName' => $detailData->product_name,
+        'quantity' => $detailData->quantity,
+        'unitPrice' => $detailData->unit_price,
+        'taxRate' => $detailData->tax_rate,
+        'totalPrice' => $detailData->total_price,
+      );
+    }
+    return $detailDatas;
   }
 }
