75e33e1b9eca8ad167be268a2881fd29bf3f704c
yoshihiro-yaguchi <yoshihiro19990307@outlook.com>
2023-11-12 10:41:19 +0900

    在庫関連テーブルの作成

---
+++ database/migrations/2023_11_12_004334_stocks.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function(Blueprint $table) {
+            $table->id();
+            // 契約ID	contract_id	varchar	8
+            $table->string('contract_id', 8)->comment('契約ID');
+            // 商品コード	production_code	varchar	50
+            $table->string('production_code', 50)->comment('商品ID');
+            // 在庫数	quantity_of_stock	integer
+            $table->decimal('quantity_of_stock', 12, 3)->comment('在庫数');
+            $table->timestamps();
+
+            // UK
+            $table->unique(['contract_id', 'production_code'], 'stocks_uk1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
---
+++ database/migrations/2023_11_12_005158_create_stocks_change_history.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('stocks_change_history', function (Blueprint $table) {
+            $table->id();
+            // 契約ID	contract_id	varchar	8
+            $table->string('contract_id', 8)->comment('契約ID');
+            // 商品コード	production_code	varchar	50
+            $table->string('production_code', 50)->comment('商品コード');
+            // 変動区分	change_classification	varchar	5
+            $table->string('change_division', 5)->comment('変動区分');
+            // 変動数	change_quantity	number
+            $table->decimal('change_quantity', 8, 3)->comment('変動数');
+            // 在庫数	quantity_of_stock	number
+            $table->decimal('quantity_of_stock', 12, 3)->comment('在庫数');
+            // メモ	memo	text
+            $table->text('memo')->comment('メモ');
+            $table->timestamps();
+
+            $table->unique(['contract_id', 'production_code', 'created_at'], 'stocks_change_history_uk1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks_change_history');
+    }
+};
